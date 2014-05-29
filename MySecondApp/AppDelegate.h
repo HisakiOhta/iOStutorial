f4afd4aae2867caa8b2152925011ff5c77ddb29e
Hisaki Ohta <hisaki@H-MBA.local>
2014-05-29 13:10:15 +0900

    Initial Commit

---
+++ MySecondApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySecondApp
+//
+//  Created by Hisaki Ohta on 2014/05/29.
+//  Copyright (c) 2014年 Hisaki Ohta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySecondApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySecondApp
+//
+//  Created by Hisaki Ohta on 2014/05/29.
+//  Copyright (c) 2014年 Hisaki Ohta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
